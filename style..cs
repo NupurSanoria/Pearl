fe95191c807a918b8496dc09caedd3c7995d769a
NupurSanoria <112044573+NupurSanoria@users.noreply.github.com>
2022-09-12 14:03:38 +0530

    Add files via upload

---
+++ style..cs
@@ -0,0 +1,62 @@
+body{
+    
+
+    font-family: 'Open Sans', sans-serif;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: c;
+    height: 100vh;
+    margin: 0;
+
+}
+
+.results {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.result {
+    background-color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    height: 500px;
+    width: 500px;
+}
+
+.result img {
+    object-fit: cover;
+    height: 500px;
+    width: 500px;
+}
+
+.buttons {
+    display: flex;
+    position:relative;
+    align-items: center;
+
+    justify-content:center;
+    background-color: grey;
+    margin:50px;
+    font-size: medium;
+
+border: 2px solid black;
+     height: 30px;
+    width:150px;
+    
+
+}
+.main{
+    position: relative;
+    margin :0;
+    padding:0;
+    
+
+
+}
+
+    
+
